d7219ae012204a3e3174fb894b8476fbac7eaf6f
RomischerChristoferEmanuel <romischer.christoferemanuel.2024d@telekimezotur.hu>
2026-09-07 14:23:02 +0200

    feltétel

--- gyakorlo/Program.cs
+++ gyakorlo/Program.cs
@@ -2,4 +2,11 @@
 Console.WriteLine("Hello, World!");
 int number = 42;
 Console.WriteLine(number);
-Console.Write("Szia!");
+Console.WriteLine("Szia!");
+
+int szam1 = 22;
+int szam2 = 44;
+
+
+if (szam1 < szam2) Console.WriteLine(szam2); 
+else Console.WriteLine(szam1);
