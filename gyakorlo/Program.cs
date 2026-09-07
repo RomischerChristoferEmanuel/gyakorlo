9132b2eae671923c2af1d49fedbcda0a7c621e7c
RomischerChristoferEmanuel <romischer.christoferemanuel.2024d@telekimezotur.hu>
2026-09-07 14:27:52 +0200

    feltétel

--- gyakorlo/Program.cs
+++ gyakorlo/Program.cs
@@ -8,5 +8,15 @@ int szam1 = 22;
 int szam2 = 44;
 
 
-if (szam1 < szam2) Console.WriteLine(szam2); 
-else Console.WriteLine(szam1);
+if (szam1 < szam2) Console.WriteLine(szam2);
+{
+    szam2 = szam2 * 2;
+    Console.WriteLine($"A nagyobb: {szam2}");
+
+}
+
+else
+{
+    szam1 = szam1 * 2;
+    Console.WriteLine(szam1);
+}
